c3708a4a455e575930544db95cb12709c3efc11d
cavion <caorendao187@163.com>
2018-03-22 22:08:15 +0800

    剔除环境参数初始化

--- rk-cloud-sysService/src/main/java/org/rk/cloud/sysservice/RkCloudSysServiceApplication.java
+++ rk-cloud-sysService/src/main/java/org/rk/cloud/sysservice/RkCloudSysServiceApplication.java
@@ -1,20 +1,14 @@
 package org.rk.cloud.sysservice;
 
-import org.rk.cloud.common.config.AppPropConfigInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class RkCloudSysServiceApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(RkCloudSysServiceApplication.class, args);
-		String env = context.getEnvironment().getProperty("spring.profiles.active");
-		String appName = context.getEnvironment().getProperty("spring.application.name");
-		AppPropConfigInfo.setEnv(env);
-		AppPropConfigInfo.setAppName(appName);
+		SpringApplication.run(RkCloudSysServiceApplication.class, args);
 	}
 }
